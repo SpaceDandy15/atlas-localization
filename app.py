71e5a1cca7c525276960becc9a67726794cff6b6
Malik Vance <malikovance@gmail.com>
2025-06-01 11:44:20 -0500

    date and time logged and working task 1

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""
-Flask app for a simple web application with internationalization (i18n) support using Babel.
-"""
+"""Flask app with Babel internationalization"""
 
 from flask import Flask, render_template
-from flask_babel import Babel
+from flask_babel import Babel, _
+from datetime import datetime
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -16,9 +15,11 @@ app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 @app.route('/')
 def index():
-    """Home route that returns a simple HTML page."""
-    return render_template('index.html')
+    """Home route with a translated greeting"""
+    greeting = _("Hello, world!")
+    time_msg = _("Current server time: %(time)s", time=datetime.utcnow().strftime("%H:%M:%S"))
+    return render_template('index.html', greeting=greeting, time_msg=time_msg)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
